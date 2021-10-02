fb3d622f85d5cfb717855257cc6542af015dcbb8
hocgin <hocgin@gmail.com>
2021-10-03 00:23:21 +0800

    feat: 推送脚本

--- scripts/pull-tag.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-git tag -a v3.1.6 -m "发布 version"
-git push origin --tags
---
+++ scripts/push-tag.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+version=$1
+version=${version:='v3.1.6'}
+
+
+
+
+echo "正在发布版本 $version"
+#git tag -a $version -m "发布 version"
+#git push origin --tags
