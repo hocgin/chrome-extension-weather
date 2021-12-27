fd7a4174251ce61d09d3d834eea2dce91f9a7dcf
hocgin <hocgin@gmail.com>
2021-12-27 17:47:07 +0800

    feat: 版本升级

--- scripts/push-tag.sh
+++ scripts/push-tag.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 version=$1
-version=${version:='v3.1.6'}
+version=${version:='v3.1.7'}
 
 
 
 
 echo "正在发布版本 $version"
-#git tag -a $version -m "发布 version"
-#git push origin --tags
+git tag -a $version -m "发布 version"
+git push origin --tags
